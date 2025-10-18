20f3adac0ad0ee4a51225b66030cdd1b03bdd20f
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-10-19 00:24:46 +0530

    made changes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from google import genai
 from dotenv import load_dotenv
-import sys 
+import sys
+from google.genai import types
+
 
 def main():
     load_dotenv()
@@ -10,17 +12,26 @@ def main():
 
     if len(sys.argv) < 2:
         print("I need a prompt")
-        sys.exit(1) 
+        sys.exit(1)
     prompt = sys.argv[1]
 
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
+
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response:")
     print(response.text)
 
 
